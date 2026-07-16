56c65bf26d41583e348ad8fe42a089bc7adda6bd
Zolan Givre <zolannp2602@gmail.com>
2026-07-16 16:06:56 +0200

    feat: update text in hero with rich text

--- src/components/ui/RichText.tsx
+++ src/components/ui/RichText.tsx
@@ -1,9 +1,13 @@
 import { RichText as PayloadRichText } from '@payloadcms/richtext-lexical/react'
 
-import type { About, Journal, Project } from '@/payload-types'
+import type { About, Hero, Journal, Project } from '@/payload-types'
 
 type RichTextProps = {
-  content: Journal['content'] | NonNullable<Project['description']> | NonNullable<About['body']>
+  content:
+    | Journal['content']
+    | NonNullable<Project['description']>
+    | NonNullable<About['body']>
+    | NonNullable<Hero['description']>
 }
 
 export function RichText({ content }: RichTextProps) {
